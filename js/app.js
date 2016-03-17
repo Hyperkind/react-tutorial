473daf336be363dc388be79e4e355ff0a7ec28e6
Jon Ah-Nee <jonahnee@gmail.com>
2016-03-16 19:50:36 -1000

    rendering classes, markup added with dangerous HTML markup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,61 @@
-document.write('it works');
+/**
+  * - CommentBox
+  *  - CommentList
+  *   - Comment
+  *  - CommentForm
+  */
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <CommentList />
+        <CommentForm />
+        Hello, world! I am a CommentBox.
+      </div>
+    )
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return(
+      <div className="commentList">
+        <Comment author="Jon">React is swell!</Comment>
+        <Comment author="Timmy">*Timmah!*</Comment>
+      </div>
+    )
+  }
+});
+
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
+  render: function() {
+    return(
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    )
+  }
+})
+
+var CommentForm = React.createClass({
+  render: function() {
+    return(
+      <div className="commentForm">
+        Hello! This is the CommentForm!
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
